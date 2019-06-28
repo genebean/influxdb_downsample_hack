1de270e9ef1d3b9b8bf53054327dd9b0d319a231
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-06-28 11:35:46 -0400

    Fixed bugs, switched to yaml answer file
    Originally the data was going to the wrong retention policy after being
    downsampled. This fixes that issue.
    
    I adapted the code to allow running against multiple databases.
    
    I also switched from the provided JSON answer file to a YAML one for
    simplicity.

--- influx-ticket-41490.sh
+++ influx-ticket-41490.sh
@@ -3,17 +3,19 @@
 # bring in the non-sharable things.
 source local_vars
 
+# To enable working with more than one db we get the db's name from stdin
+export DB_NAME=$1
+export RP_NAME=$2
+
 ### Step One --- cleaning out existing jobs
 ### First line, deletes all the existing kapacitor scripts that start with 'hack'
 ### Change as you see fit, I just wanted a way to differentiate the ones
 ### that are made with this script from others
 
-kapacitor list tasks | grep -e '^hack_' | awk '{print $1}' > $WORKING_DIR/tmp/kapatmp.txt
+kapacitor list tasks | grep "hack_${DB_NAME}_" | awk '{print $1}' > $WORKING_DIR/tmp/kapatmp.txt
 
 for OLDKAP in $(cat $WORKING_DIR/tmp/kapatmp.txt);do
-
-kapacitor delete tasks $OLDKAP
-
+  kapacitor delete tasks $OLDKAP
 done
 
 ### Step Two --- getting the measurements
@@ -22,17 +24,18 @@ done
 influx -host $INFLUX_URL -port 8086 -username $INFLUX_USERNAME -password $INFLUX_PASSWORD -ssl --execute "show measurements on $DB_NAME" > $WORKING_DIR/tmp/dbtmp.txt
 
 for MEASUREMENT in $(tail -n +4 $WORKING_DIR/tmp/dbtmp.txt );do
+  ### Alters a stock .json file to create specific ones for the measurement in the loop
+  cat $WORKING_DIR/hack_empty.yaml \
+  | sed "s/replace_with_db/"$DB_NAME"/g" \
+  | sed "s/replace_with_rp/"$RP_NAME"/g" \
+  | sed "s/replace_with_measurement/"$MEASUREMENT"/g" \
+  > "tmp/hack_${DB_NAME}_${MEASUREMENT}.yaml"
 
-### Alters a stock .json file to create specific ones for the measurement in the loop
-cat $WORKING_DIR/hack_empty.json | sed "s/empty/"$MEASUREMENT"/g" > tmp/hack_$MEASUREMENT.json
-
-### Does the kapacitor mojo to create the task and enable it
-kapacitor define hack_$MEASUREMENT -file $WORKING_DIR/tmp/hack_$MEASUREMENT.json
-kapacitor enable hack_$MEASUREMENT
-
+  ### Does the kapacitor mojo to create the task and enable it
+  kapacitor define "hack_${DB_NAME}_${MEASUREMENT}" -file "${WORKING_DIR}/tmp/hack_${DB_NAME}_${MEASUREMENT}.yaml"
+  kapacitor enable "hack_${DB_NAME}_${MEASUREMENT}"
 done
 
 
 ### Lists all tasks when done
 kapacitor list tasks
-
